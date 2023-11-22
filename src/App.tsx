cde8a2eacbaadc0de1863c2ccb014b4c10b64feb
Camden Withrow <camdenwithrow@Camdens-MacBook-Pro.local>
2023-11-22 16:36:34 -0500

    handle input and storing values

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect, useRef, useCallback, ChangeEvent } from "react"
 
 interface Cell {
   value: string
@@ -11,13 +11,13 @@ interface CellRef {
 
 const numRows = 100
 const numCols = 26
-const rowSize = "25px"
+const rowSize = "30px"
 const colSize = "100px"
 
 const initialCells = () => {
-  let cells = []
+  const cells = []
   for (let i = 0; i < numRows; i++) {
-    let row = []
+    const row = []
     for (let j = 0; j < numCols; j++) {
       row.push({ value: "" })
     }
@@ -29,123 +29,134 @@ const initialCells = () => {
 function App() {
   const [cells, setCells] = useState<Cell[][]>(initialCells())
   const [activeCell, setActiveCell] = useState<CellRef>({ row: 0, col: 0 })
-  const [isEditing, setIsEditing] = useState<boolean>(true)
-  const [inputVal, setInputVal] = useState("1")
-  const [shiftKey, setShiftKey] = useState(false)
+  const [isEditing, setIsEditing] = useState<boolean>(false)
+
+  const inputRef = useRef<HTMLInputElement | null>(null)
+
+  const colHeaders = useCallback(() => {
+    const headers = []
+    for (let index = 0; index < numCols; index++) {
+      let columnName = ""
+      let i = index
+      while (i >= 0) {
+        columnName = String.fromCharCode(65 + (i % 26)) + columnName
+        i = Math.floor(i / 26) - 1
+      }
+      headers.push(columnName)
+    }
+    return headers
+  }, [])
+
+  const storeVal = useCallback(
+    (val?: string) => {
+      if (val !== undefined) {
+        setCells((prevCells) => {
+          const newCells = [...prevCells]
+          newCells[activeCell.row][activeCell.col].value = val
+          return newCells
+        })
+      }
+      setIsEditing(false)
+    },
+    [activeCell.row, activeCell.col]
+  )
+
+  const editCell = () => {
+    setIsEditing(true)
+  }
+
+  useEffect(() => {
+    if (isEditing && inputRef.current) {
+      inputRef.current.focus()
+    }
+  }, [isEditing])
 
   useEffect(() => {
     const navigateCells = (rowDelta: number, colDelta: number) => {
-      let newRow = activeCell.row + rowDelta
-      let newCol = activeCell.col + colDelta
+      const newRow = activeCell.row + rowDelta
+      const newCol = activeCell.col + colDelta
       if (newRow >= 0 && newRow < numRows && newCol >= 0 && newCol < numCols) {
         setActiveCell({ row: newRow, col: newCol })
       }
     }
 
-    const handleKeyUp = (e: KeyboardEvent) => {
-      if (e.key === "Shift") {
-        setShiftKey(false)
-      }
-    }
-
     const handleKeyDown = (e: KeyboardEvent) => {
-      switch (e.key) {
-        case "Shift":
-          e.preventDefault()
-          setShiftKey(true)
-          break
-        case "Enter":
-          e.preventDefault()
-          if (isEditing) {
-            storeInput()
-            const rowChange = shiftKey ? -1 : 1
-            navigateCells(rowChange, 0)
-          } else {
-            editCell()
-          }
-          break
-        case "Tab":
-          e.preventDefault()
-          if (isEditing) storeInput()
-          const colChange = shiftKey ? -1 : 1
-          navigateCells(0, colChange)
-          break
-        case "ArrowUp":
-          e.preventDefault()
-          if (!isEditing) {
-            navigateCells(-1, 0)
-          }
-          break
-        case "ArrowDown":
-          e.preventDefault()
-          if (!isEditing) {
-            navigateCells(1, 0)
-          }
-          break
-        case "ArrowLeft":
-          e.preventDefault()
-          if (!isEditing) {
-            navigateCells(0, -1)
-          }
-          break
-        case "ArrowRight":
-          e.preventDefault()
-          if (!isEditing) {
-            navigateCells(0, 1)
-          }
-          break
-        case "Backspace":
-          if (!isEditing) {
-            //
-          }
-          break
-        default:
-          console.log(e.key)
-          if (e.key.length === 1 && !isEditing) {
+      const isModifierKey = e.metaKey || e.altKey || e.ctrlKey
+      if (!isEditing && e.key.length === 1 && !isModifierKey) {
+        e.preventDefault()
+        setIsEditing(true)
+        setCells((prevCells) => {
+          const newCells = [...prevCells]
+          newCells[activeCell.row][activeCell.col].value = e.key
+          return newCells
+        })
+      } else {
+        let colChange = 0
+        switch (e.key) {
+          case "Enter":
             e.preventDefault()
-            setIsEditing(true)
-            setInputVal(e.key)
-          }
-          break
+            if (isEditing) {
+              storeVal()
+              const rowChange = e.shiftKey ? -1 : 1
+              navigateCells(rowChange, 0)
+            } else {
+              editCell()
+            }
+            break
+          case "Tab":
+            e.preventDefault()
+            if (isEditing) storeVal()
+            colChange = e.shiftKey ? -1 : 1
+            navigateCells(0, colChange)
+            break
+          case "ArrowUp":
+            e.preventDefault()
+            if (!isEditing) {
+              navigateCells(-1, 0)
+            }
+            break
+          case "ArrowDown":
+            e.preventDefault()
+            if (!isEditing) {
+              navigateCells(1, 0)
+            }
+            break
+          case "ArrowLeft":
+            e.preventDefault()
+            if (!isEditing) {
+              navigateCells(0, -1)
+            }
+            break
+          case "ArrowRight":
+            e.preventDefault()
+            if (!isEditing) {
+              navigateCells(0, 1)
+            }
+            break
+          case "Backspace":
+            if (!isEditing) {
+              e.preventDefault()
+              storeVal("")
+            }
+            break
+          case "Escape":
+            if (isEditing) {
+              e.preventDefault()
+              storeVal("")
+            }
+            break
+          default:
+            break
+        }
       }
     }
 
     window.addEventListener("keydown", handleKeyDown)
-    window.addEventListener("keyup", handleKeyUp)
 
     return () => {
       window.removeEventListener("keydown", handleKeyDown)
-      window.removeEventListener("keyup", handleKeyUp)
     }
-  }, [activeCell, setActiveCell, shiftKey, setShiftKey, isEditing])
-
-  const genColHeaders = (index: number) => {
-    let columnName = ""
-    while (index >= 0) {
-      columnName = String.fromCharCode(65 + (index % 26)) + columnName
-      index = Math.floor(index / 26) - 1
-    }
-    return columnName
-  }
-
-  const storeInput = () => {
-    console.log("store")
-    if (inputVal !== cells[activeCell.row][activeCell.col].value) {
-      const newCells = [...cells]
-      newCells[activeCell.row][activeCell.col].value = inputVal
-      setCells(newCells)
-    }
-    setInputVal("")
-    setIsEditing(false)
-  }
-
-  const editCell = () => {
-    console.log("edit")
-    if (inputVal !== cells[activeCell.row][activeCell.col].value) {
-      setInputVal(cells[activeCell.row][activeCell.col].value)
-    }
-    setIsEditing(true)
-  }
+  }, [activeCell, setActiveCell, isEditing, storeVal])
 
   const isActiveCell = (row: number, col: number) => {
     return activeCell.row === row && activeCell.col === col
@@ -155,9 +166,17 @@ function App() {
     if (isActiveCell(row, col)) {
       editCell()
     } else {
-      storeInput()
+      storeVal()
       setActiveCell({ row: row, col: col })
     }
+  }
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setCells((prevCells) => {
+      const newCells = [...prevCells]
+      newCells[activeCell.row][activeCell.col].value = e.target.value
+      return newCells
+    })
   }
 
   return (
@@ -166,8 +185,11 @@ function App() {
         <tbody>
           <tr>
             <th className="opacity-0">0</th>
+            {colHeaders().map((colH, colIndex) => (
+              <th key={`colHeader-${colIndex}`}>{colH}</th>
+            ))}
             {Array.from({ length: numCols }, (_, colIndex) => (
-              <th key={`colHeader-${colIndex}`}>{genColHeaders(colIndex)}</th>
+              <th key={`colHeader-${colIndex}`}>{colHeaders()}</th>
             ))}
           </tr>
           {cells.map((row, rowIndex) => (
@@ -177,10 +199,11 @@ function App() {
                 <td
                   key={colIndex}
                   className={`border p-0 ${
-                    activeCell.row === rowIndex && activeCell.col === colIndex ? "bg-blue-200" : ""
+                    activeCell.row === rowIndex && activeCell.col === colIndex ? "border-2 border-blue-300" : ""
                   }`}
                 >
                   <div
+                    className="p-0.5"
                     style={{
                       minWidth: colSize,
                       minHeight: rowSize,
@@ -189,7 +212,17 @@ function App() {
                     }}
                     onClick={() => handleCellClick(rowIndex, colIndex)}
                   >
-                    {isEditing && isActiveCell(rowIndex, colIndex) ? <div>{inputVal}</div> : <p>{cell.value}</p>}
+                    {isEditing && activeCell.row === rowIndex && activeCell.col === colIndex ? (
+                      <input
+                        value={cells[activeCell.row][activeCell.col].value}
+                        ref={inputRef}
+                        onChange={handleInputChange}
+                        onBlur={() => storeVal()}
+                        className="w-full h-full border-0 outline-none"
+                      />
+                    ) : (
+                      <p className={`w-full overflow-hidden`}>{cell.value}</p>
+                    )}
                   </div>
                 </td>
               ))}
