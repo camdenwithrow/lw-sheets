ff711b5dfc40e00839015e505380caf077a118a1
Camden Withrow <camdenwithrow@Camdens-MacBook-Pro.local>
2023-11-22 12:17:00 -0500

    initial store cell from input and edit cell

--- src/App.tsx
+++ src/App.tsx
@@ -14,14 +14,27 @@ const numCols = 26
 const rowSize = "25px"
 const colSize = "100px"
 
+const initialCells = () => {
+  let cells = []
+  for (let i = 0; i < numRows; i++) {
+    let row = []
+    for (let j = 0; j < numCols; j++) {
+      row.push({ value: "" })
+    }
+    cells.push(row)
+  }
+  return cells
+}
+
 function App() {
-  const [cells, setCells] = useState<Cell[][]>(Array(numRows).fill(Array(numCols).fill({ value: "" })))
+  const [cells, setCells] = useState<Cell[][]>(initialCells())
   const [activeCell, setActiveCell] = useState<CellRef>({ row: 0, col: 0 })
   const [isEditing, setIsEditing] = useState<boolean>(true)
+  const [inputVal, setInputVal] = useState("1")
   const [shiftKey, setShiftKey] = useState(false)
 
   useEffect(() => {
-    const navigateCells = (e: KeyboardEvent, rowDelta: number, colDelta: number) => {
+    const navigateCells = (rowDelta: number, colDelta: number) => {
       let newRow = activeCell.row + rowDelta
       let newCol = activeCell.col + colDelta
       if (newRow >= 0 && newRow < numRows && newCol >= 0 && newCol < numCols) {
@@ -44,40 +57,54 @@ function App() {
         case "Enter":
           e.preventDefault()
           if (isEditing) {
-            setIsEditing(false)
+            storeInput()
             const rowChange = shiftKey ? -1 : 1
-            navigateCells(e, rowChange, 0)
+            navigateCells(rowChange, 0)
           } else {
-            setIsEditing(true)
+            editCell()
           }
           break
         case "Tab":
           e.preventDefault()
+          if (isEditing) storeInput()
           const colChange = shiftKey ? -1 : 1
-          navigateCells(e, 0, colChange)
+          navigateCells(0, colChange)
           break
         case "ArrowUp":
           e.preventDefault()
           if (!isEditing) {
-            navigateCells(e, -1, 0)
+            navigateCells(-1, 0)
           }
           break
         case "ArrowDown":
           e.preventDefault()
           if (!isEditing) {
-            navigateCells(e, 1, 0)
+            navigateCells(1, 0)
           }
           break
         case "ArrowLeft":
           e.preventDefault()
           if (!isEditing) {
-            navigateCells(e, 0, -1)
+            navigateCells(0, -1)
           }
           break
         case "ArrowRight":
           e.preventDefault()
           if (!isEditing) {
-            navigateCells(e, 0, 1)
+            navigateCells(0, 1)
+          }
+          break
+        case "Backspace":
+          if (!isEditing) {
+            //
+          }
+          break
+        default:
+          console.log(e.key)
+          if (e.key.length === 1 && !isEditing) {
+            e.preventDefault()
+            setIsEditing(true)
+            setInputVal(e.key)
           }
           break
       }
@@ -101,8 +128,36 @@ function App() {
     return columnName
   }
 
+  const storeInput = () => {
+    console.log("store")
+    if (inputVal !== cells[activeCell.row][activeCell.col].value) {
+      const newCells = [...cells]
+      newCells[activeCell.row][activeCell.col].value = inputVal
+      setCells(newCells)
+    }
+    setInputVal("")
+    setIsEditing(false)
+  }
+
+  const editCell = () => {
+    console.log("edit")
+    if (inputVal !== cells[activeCell.row][activeCell.col].value) {
+      setInputVal(cells[activeCell.row][activeCell.col].value)
+    }
+    setIsEditing(true)
+  }
+
   const isActiveCell = (row: number, col: number) => {
     return activeCell.row === row && activeCell.col === col
+  }
+
+  const handleCellClick = (row: number, col: number) => {
+    if (isActiveCell(row, col)) {
+      editCell()
+    } else {
+      storeInput()
+      setActiveCell({ row: row, col: col })
+    }
   }
 
   return (
@@ -121,7 +176,7 @@ function App() {
               {row.map((cell, colIndex) => (
                 <td
                   key={colIndex}
-                  className={`border p-1 ${
+                  className={`border p-0 ${
                     activeCell.row === rowIndex && activeCell.col === colIndex ? "bg-blue-200" : ""
                   }`}
                 >
@@ -132,8 +187,9 @@ function App() {
                       width: colSize,
                       height: rowSize,
                     }}
+                    onClick={() => handleCellClick(rowIndex, colIndex)}
                   >
-                    {isEditing && isActiveCell(rowIndex, colIndex) ? <div>Editing</div> : <p>{cell.value}</p>}
+                    {isEditing && isActiveCell(rowIndex, colIndex) ? <div>{inputVal}</div> : <p>{cell.value}</p>}
                   </div>
                 </td>
               ))}
