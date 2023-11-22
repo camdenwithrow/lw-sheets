3e5bad30e6e7be5a81f104f6ee2d72d585338fab
Camden Withrow <camdenwithrow@Camdens-MacBook-Pro.local>
2023-11-22 11:21:10 -0500

    add keyboard navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
 interface Cell {
   value: string
-  isEditing: boolean
 }
 
 interface CellRef {
@@ -16,10 +15,64 @@ const rowSize = "25px"
 const colSize = "100px"
 
 function App() {
-  const [cells, setCells] = useState<Cell[][]>(
-    Array(numRows).fill(Array(numCols).fill({ value: "", isEditing: false }))
-  )
+  const [cells, setCells] = useState<Cell[][]>(Array(numRows).fill(Array(numCols).fill({ value: "" })))
   const [activeCell, setActiveCell] = useState<CellRef>({ row: 0, col: 0 })
+  const [isEditing, setIsEditing] = useState<boolean>(false)
+  const [shiftKey, setShiftKey] = useState(false)
+
+  useEffect(() => {
+    const navigateCells = (e: KeyboardEvent, rowDelta: number, colDelta: number) => {
+      e.preventDefault()
+      let newRow = activeCell.row + rowDelta
+      let newCol = activeCell.col + colDelta
+      if (newRow >= 0 && newRow < numRows && newCol >= 0 && newCol < numCols) {
+        setActiveCell({ row: newRow, col: newCol })
+      }
+    }
+
+    const handleKeyUp = (e: KeyboardEvent) => {
+      if (e.key === "Shift") {
+        setShiftKey(false)
+      }
+    }
+
+    const handleKeyDown = (e: KeyboardEvent) => {
+      switch (e.key) {
+        case "Shift":
+          e.preventDefault()
+          setShiftKey(true)
+          break
+        case "Enter":
+          const rowChange = shiftKey ? -1 : 1
+          navigateCells(e, rowChange, 0)
+          break
+        case "Tab":
+          const colChange = shiftKey ? -1 : 1
+          navigateCells(e, 0, colChange)
+          break
+        case "ArrowUp":
+          navigateCells(e, -1, 0)
+          break
+        case "ArrowDown":
+          navigateCells(e, 1, 0)
+          break
+        case "ArrowLeft":
+          navigateCells(e, 0, -1)
+          break
+        case "ArrowRight":
+          navigateCells(e, 0, 1)
+          break
+      }
+    }
+
+    window.addEventListener("keydown", handleKeyDown)
+    window.addEventListener("keyup", handleKeyUp)
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown)
+      window.removeEventListener("keyup", handleKeyUp)
+    }
+  }, [activeCell, setActiveCell, shiftKey, setShiftKey])
 
   const genColHeaders = (index: number) => {
     let columnName = ""
@@ -34,10 +87,12 @@ function App() {
     <main>
       <table className="table-auto border-collapse">
         <tbody>
-          <th className="opacity-0">0</th>
-          {Array.from({ length: numCols }, (_, colIndex) => (
-            <th>{genColHeaders(colIndex)}</th>
-          ))}
+          <tr>
+            <th className="opacity-0">0</th>
+            {Array.from({ length: numCols }, (_, colIndex) => (
+              <th key={`colHeader-${colIndex}`}>{genColHeaders(colIndex)}</th>
+            ))}
+          </tr>
           {cells.map((row, rowIndex) => (
             <tr key={rowIndex}>
               <th>{rowIndex + 1}</th>
@@ -45,9 +100,7 @@ function App() {
                 <td
                   key={colIndex}
                   className={`border p-1 ${
-                    activeCell.row === rowIndex && activeCell.col === colIndex
-                      ? "bg-blue-200"
-                      : ""
+                    activeCell.row === rowIndex && activeCell.col === colIndex ? "bg-blue-200" : ""
                   }`}
                 >
                   <div
